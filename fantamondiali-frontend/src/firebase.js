7cf8e5d8530ca68ba626c2804a515151315dbb58
lorenzocandela <lorenzocandela01@gmail.com>
2026-03-19 14:08:52 +0000

    Sicurezza: spostata API Key Firebase in .env

--- fantamondiali-frontend/src/firebase.js
+++ fantamondiali-frontend/src/firebase.js
@@ -1,9 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// I dati copiati dalla tua console Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAUlVRrqZg8qL6_eYwSrp0czllt2IHL0eg",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "fantamondiali-e1f5c.firebaseapp.com",
   projectId: "fantamondiali-e1f5c",
   storageBucket: "fantamondiali-e1f5c.firebasestorage.app",
@@ -11,8 +10,6 @@ const firebaseConfig = {
   appId: "1:607003325581:web:ac19cf13f7e5e401f75ec4"
 };
 
-// Inizializza Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inizializza l'autenticazione ed esportala
 export const auth = getAuth(app);
