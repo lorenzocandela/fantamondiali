c213aa6c4f6c89f83e6b755eef770dfcfb7fe0f2
Lorenzo Candela <lorenzocandela01@gmail.com>
2026-03-23 20:44:57 +0100

    fix

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                     <span id="error-text"></span>
                 </div>
                 <button id="btn-submit" class="btn-cta">
-                    <span id="btn-label">ACCEDI</span>
+                    <span id="btn-label">Accedi</span>
                 </button>
             </div>
         </div>
